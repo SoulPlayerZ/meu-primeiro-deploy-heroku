d7468516d4e684e76a248fada7d478e1a1a02ec1
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-23 19:27:32 -0300

    Adiciona Topico_4

---
+++ bloco_4_Introducao_ao_JavaScript/dia_1/exercicios_do_dia/Topico_4.js
@@ -0,0 +1,9 @@
+let valor = 0;
+
+if(valor > 0){
+    console.log("O valor é Positivo");
+}else if(valor < 0){
+    console.log("O valor é Negativo");
+}else{
+    console.log("O valor é igual a ZERO");
+}
