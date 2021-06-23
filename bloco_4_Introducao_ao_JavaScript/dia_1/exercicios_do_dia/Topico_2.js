345ae50e285d8ed67be799f4bb7ce92532dcaf0a
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-23 18:50:52 -0300

    Adiciona Topico_2 aos exercicios do dia 4. Remove a pasta trybe-exercises(testes bagunçados)

---
+++ bloco_4_Introducao_ao_JavaScript/dia_1/exercicios_do_dia/Topico_2.js
@@ -0,0 +1,10 @@
+let valor1 = 6;
+let valor2 = 6;
+
+if  (valor1 > valor2){
+    console.log("Valor1 ( ", valor1, " ) ", "é maior que Valor2 ( ", valor2, " ) ");
+}else if (valor1 < valor2){
+    console.log("Valor1 ( ", valor1, " ) ", "é menor que Valor2 ( ", valor2, " ) ");
+}else{
+    console.log("Valor1 ( ", valor1, " ) ", "é igual a Valor2 ( ", valor2, " ) ");
+}
