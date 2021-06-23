c55aeaa521bdaa1dd0f3ed0812c102ee25b7729d
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-23 20:16:42 -0300

    Adiciona Topico_5(Exercicio Triangulo)

---
+++ bloco_4_Introducao_ao_JavaScript/dia_1/exercicios_do_dia/Topico_5.js
@@ -0,0 +1,12 @@
+let v1 = 0;
+let v2 = 0;
+let v3 = 0;
+
+if((v1 + v2 + v3) <= 0){
+    console.log("ERRO.");
+}else if((v1 + v2 + v3) === 180){
+    console.log("TRUE");
+}else{
+    console.log("FALSE");
+}
+
