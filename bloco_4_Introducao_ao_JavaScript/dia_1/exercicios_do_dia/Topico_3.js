ca34aa11681e7ece099c4c2bd418b943e105b6c6
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-23 19:20:07 -0300

    Adiciona Topico_3.js

---
+++ bloco_4_Introducao_ao_JavaScript/dia_1/exercicios_do_dia/Topico_3.js
@@ -0,0 +1,13 @@
+let valor1 = 8;
+let valor2 = 80;
+let valor3 = 80;
+
+if  (valor1 > valor2 && valor3){
+    console.log("O maior valor é: ", valor1);
+}else if(valor2 > valor1 && valor3){
+    console.log("O maior valor é: ", valor2);
+}else if(valor3 > valor1 && valor2){
+    console.log("O maior valor é: ", valor3);
+}else{
+    console.log("Os três valores são iguais");
+}
