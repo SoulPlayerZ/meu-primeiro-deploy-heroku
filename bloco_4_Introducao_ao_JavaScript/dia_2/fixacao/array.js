42ada4d876cc50dd711dd44cb75d366b2625735b
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-24 15:18:44 -0300

    Adiciona  exercicios de fixaçao ARRAYS do DIA 2

---
+++ bloco_4_Introducao_ao_JavaScript/dia_2/fixacao/array.js
@@ -0,0 +1,48 @@
+/* Exercício 3
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("Contato");
+console.log(menu); */
+
+
+
+
+
+
+
+/* Exercício 2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio); */
+
+
+
+
+/* Exercício 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+ */
+
+
+
+
+/* Ainda do conteúdo.
+
+
+let tasksList = ["Tomar café", 'Reunião', 'Brincar com o cachorro'];
+
+    tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+    tasksList.push(23);
+    tasksList.unshift("Jogar Valorant");
+    tasksList.pop();
+    tasksList.shift();
+    tasksList.pop();
+    console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe'] */
