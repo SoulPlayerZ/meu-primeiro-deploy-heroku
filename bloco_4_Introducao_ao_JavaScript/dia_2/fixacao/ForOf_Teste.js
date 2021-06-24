c21b89107bc6b4f6cf8d86e312ad715737561a1c
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-24 15:52:53 -0300

    Adiciona exercicio de FIXACAO FOR/OF ao DIA 2

---
+++ bloco_4_Introducao_ao_JavaScript/dia_2/fixacao/ForOf_Teste.js
@@ -0,0 +1,13 @@
+
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+    numero += 1;
+    console.log(numero);
+}
+
+// resultado: 
+//1
+//2
+//3
+//4
+//5
---
+++ bloco_4_Introducao_ao_JavaScript/dia_2/fixacao/forOf.js
@@ -0,0 +1,4 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let N of names){
+    console.log(N);
+}
