6d5aa72a28206ad8dfa12c65cef94e43b1219198
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-06-28 13:47:47 -0300

    Adiciona exercicios de fixaçao sobre OBJETOS. Bloco 4.4

---
+++ bloco_4_Introducao_ao_JavaScript/dia_3/exercicios_do_dia.js
@@ -0,0 +1,42 @@
+
+let size = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + symbol;
+};
+ 
+
+
+/* Exercício 2 
+
+
+let valor = 5;
+let asterisco = "*";
+let espaco = "";
+
+for(let index = 1; index <= valor; index += 1){
+    espaco = espaco + asterisco;
+    console.log(espaco);
+} */
+
+
+
+
+/* Exercício 1
+
+let valor = 4;
+let asterisco = "*";
+let espaco = "";
+
+for(let index = 1; index <= valor; index += 1){
+    espaco = espaco + asterisco;
+}
+
+for(let index = 1; index <= valor; index += 1){
+    console.log(espaco);
+}
+    
+     */
