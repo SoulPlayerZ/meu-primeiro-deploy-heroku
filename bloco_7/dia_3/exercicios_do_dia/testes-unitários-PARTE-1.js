1f024afff56971a434274ba12fc398fa345588fb
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-26 19:03:07 -0300

    Bloco 7.3 PARTE 1.2 EXERCICIOS DO DIA

--- bloco_7/dia_3/exercicios_do_dia/testes-unitários-PARTE-1.js
+++ bloco_7/dia_3/exercicios_do_dia/testes-unitários-PARTE-1.js
@@ -1,3 +1,5 @@
+/* ----------Questão 1-------------------
+
 const assert = require('assert');
 
 function sum(a, b) {
@@ -8,11 +10,42 @@ function sum(a, b) {
   return a + b;
 }
 // implemente seus testes aqui
+//assert.strictEqual(typeof sum, 'function');
 //assert.strictEqual(sum(4, 5), 9, 'Esperado receber resultado 9');
 //assert.strictEqual(sum(0,0), 0, 'Esperado receber resultado 0');
 assert.throws(() => { sum(4, '5'); }, /^Error: parameters must be numbers$/); 
 
 sum(4,'768');
+ */
 
+
+
+/* ----------Questão 2-------------------*/
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+//assert.strictEqual(typeof myRemove, 'function');
+//1
+/* assert.deepStrictEqual(myRemove([1,2,3,4], 3), [1,2,4], 'Esperado encontrar [1,2,4] mas isso não acontece'); */
+//2
+//assert.notDeepStrictEqual(myRemove([1,2,3,4], 3), [1,2,3,4], 'O retorno precisa ser diferente de [1,2,3,4]');
+//3
+//assert.notDeepStrictEqual(myRemove([1,2,3,4], 3), [1,2,3,4], 'O retorno precisa ser igual a [1,2,3,4]');
+//4 
+//assert.deepStrictEqual(myRemove([1,2,3,4], 5), [1,2,3,4], 'O retorno precisa ser igual a [1,2,3,4]'); 
+
+
+
+/* console.log(myRemove([1,2,3,4])); */
 
 
