ffce6a8e62e10abd2d14816b7d71c2054d165db5
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-28 14:25:29 -0300

    Fixacao COMPLETO  HOF bloco 8.1

--- bloco_8/dia_1/fixacao/HOF-fixacao.js
+++ bloco_8/dia_1/fixacao/HOF-fixacao.js
@@ -1,10 +1,20 @@
+//QUESTÃO 1
+
 // Ao chamar a função doingThings:
 
-const wakeUp = () => 'Acordando!!'
+const wakeUp = () => 'Acordando!!';
+const coffee = () => 'Bora tomar café!!';
+const sleep = () => 'Partiu dormir!!';
+
 const doingThings = (func) => console.log(func());
 
-
 doingThings(wakeUp);
+doingThings(coffee);
+doingThings(sleep);
+
 
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Acordando!!
+
+
+
