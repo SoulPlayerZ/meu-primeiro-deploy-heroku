01662664c6d5eb287c8c7b4fee1f1081f861dfa9
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-28 15:23:34 -0300

    Exercicios do dia QUESTAO 1  HOF bloco 8.1

---
+++ bloco_8/dia_1/exercicios_do_dia/HOF-exercicios-do-dia-1 .js
@@ -0,0 +1,20 @@
+const pessoa = (nome1, lastName) => {
+   const p = {
+      name: `${nome1} ${lastName}`,
+      email: nome1 + lastName + '@trybe.com',
+    };
+    return p;
+};
+
+
+const newEmployees = (nomeC) => {
+    const employees = {
+      id1: nomeC('Ardrubal', 'Rodrigues'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: nomeC('Enzo', 'Paladino'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: nomeC('Astolfo', 'Miguel'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+
+  console.log(newEmployees(pessoa));
