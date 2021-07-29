7e546f4a95f5ca570e5ba7c743f081af4e34e4de
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-29 13:28:05 -0300

    Adiciona fixacao FIND  QUESTAO 2 BLOCO 8.2

--- bloco_8/dia_2/fixacao/find-fixacao-1.js
+++ bloco_8/dia_2/fixacao/find-fixacao-1.js
@@ -1,5 +1,11 @@
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+/* const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = (item) =>  (item % 3 === 0) && (item % 5 === 0);
 
-console.log(numbers.find(findDivisibleBy3And5));
+console.log(numbers.find(findDivisibleBy3And5)); */
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (cadaNome) => cadaNome.length === 5;
+
+console.log(names.find(findNameWithFiveLetters));
