34e86373edf0ac191704af2784b6f377e6f33033
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-30 13:40:29 -0300

    Adiciona MAP FIXACAO BLOCO 8.3

---
+++ bloco_8/dia_3/fixacao/MAP-fixaca0-1.js
@@ -0,0 +1,26 @@
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+
+const juntaArray = (prod, price) =>  prod.map((itemProd, indexPrice) => ( { [itemProd]: price[indexPrice], } ));
+const final = juntaArray(products, prices);
+
+
+console.log(final);
+
+
+
+/* 
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ] */
