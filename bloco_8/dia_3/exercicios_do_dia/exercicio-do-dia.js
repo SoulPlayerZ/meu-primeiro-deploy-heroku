e448ecb1fb4181caf8d749e2276b969241fec0c1
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-30 16:10:53 -0300

    Adiciona exercicios do dia QUESTAO 4

--- bloco_8/dia_3/exercicios_do_dia/exercicio-do-dia.js
+++ bloco_8/dia_3/exercicios_do_dia/exercicio-do-dia.js
@@ -123,7 +123,7 @@ const books = [
 
 
  //Questão 3
-
+/* 
  const expectedResult = [
     { 
       id: 1,
@@ -159,4 +159,37 @@ const books = [
     return books.filter((item) => ((item.genre === 'Ficção Científica') || (item.genre === 'Fantasia')));
   }
   
-  assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+  assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult); */
+
+  //Questão 4
+  const expectedResult = [
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+      releaseYear: 1928,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
+      releaseYear: 1951,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954,
+    },
+  ];
+  
+  function oldBooksOrdered() {
+    // escreva seu código aqui
+    return books.filter((item) => item.releaseYear <= 1961).sort((a, b) => (a.releaseYear - b.releaseYear));
+    
+  }
+  
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
