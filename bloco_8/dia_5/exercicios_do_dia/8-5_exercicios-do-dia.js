3a61fb5ac670db631df0f8057dd29c91a88b0522
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-03 18:57:55 -0300

    Exercicios do dia 8.5 QUESTAO 2

--- bloco_8/dia_5/exercicios_do_dia/8-5_exercicios-do-dia.js
+++ bloco_8/dia_5/exercicios_do_dia/8-5_exercicios-do-dia.js
@@ -1,14 +1,35 @@
-const assert = require('assert');
+// QUESTÃO 1 
 
-const rectangleArea = (width, height) => width * height;
+// const assert = require('assert');
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [...rectangle1, ...rectangle2, ...rectangle3];
+// const rectangleArea = (width, height) => width * height;
 
-console.log(rectangles);
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [...rectangle1, ...rectangle2, ...rectangle3];
 
- rectangles.forEach((rectangle) => {
-  assert.strictEqual(rectangleArea(rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
-}); 
+// console.log(rectangles);
+
+//  rectangles.forEach((rectangle) => {
+//   assert.strictEqual(rectangleArea(rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+// }); 
+
+
+// // Questão 2
+// const assert = require('assert');
+
+// // escreva sum abaixo
+// const sum = (...rest) => rest.reduce((acc, cur) => acc + cur, 0);
+
+// console.log(sum());
+
+
+// assert.strictEqual(sum(), 0);
+// assert.strictEqual(sum(1), 1);
+// assert.strictEqual(sum(1, 2), 3);
+// assert.strictEqual(sum(1, 2, 3), 6);
+// assert.strictEqual(sum(1, 2, 3, 4), 10);
+
+
+//Questão 3
