3fa2467f0542fb6e94876675562fdc6eb135a437
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-03 19:55:47 -0300

    Exercicios do dia 8.5 QUESTAO 4

--- bloco_8/dia_5/exercicios_do_dia/8-5_exercicios-do-dia.js
+++ bloco_8/dia_5/exercicios_do_dia/8-5_exercicios-do-dia.js
@@ -56,3 +56,50 @@ console.log(personLikes(alex));
 
 assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
 assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'); */
+
+// Questão 4
+/* const assert = require('assert');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+
+const filterPeople = (arraypeople) => {
+  return  arraypeople.filter(({ bornIn, nationality}) => nationality === 'Australian' && bornIn >= 1901 && bornIn <= 2000,);
+}
+
+const filteredPeople = filterPeople(people);
+
+console.log(filteredPeople);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' }); */
+
+//Questão 5
+
