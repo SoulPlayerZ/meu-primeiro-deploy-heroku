ee024d2a4abb515cc66c8c430292a68fa5a8c4f0
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-03 16:08:09 -0300

    Fixacao 8.5 spread

--- bloco_8/dia_5/fixacao/8-5-fixacao.js
+++ bloco_8/dia_5/fixacao/8-5-fixacao.js
@@ -28,3 +28,16 @@ console.log(fruitSalad(specialFruit, additionalItens));
 
 
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+
+/* console.log(numerosPares); // [6, 8, 10, 12]; */
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,,...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
