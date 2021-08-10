15fa945ddb4efd8b727f7a2868c5d1450ff19e5c
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-10 15:21:28 -0300

    Fixacao bloco 9.1 LIDANDO COM ERROS EM OPERAÇOES ASSINCRONAS

--- bloco_9/fixacao/fixacao.js
+++ bloco_9/fixacao/fixacao.js
@@ -24,8 +24,8 @@ const getUser = callback => {
 assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
 assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser */
 
-// QUESTÃO 2
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// QUESTÃO 2 CALLBACK
+/* const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
@@ -42,4 +42,38 @@ const getUser = (callback) => {
 };
 
 getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo */
+
+
+// Lidando com erros em operações assíncronas 
+// QUESTÃO 1
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, callbackError) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      callbackError(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
