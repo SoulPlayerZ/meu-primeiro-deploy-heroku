aed3bd9f45459e24b5e20e9788fe257243fa0d79
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-10 18:45:45 -0300

    Exercicios do dia BLOCO_9.1 QUestao 3

--- bloco_9/exercicios_do_dia/exercicios_do_dia.js
+++ bloco_9/exercicios_do_dia/exercicios_do_dia.js
@@ -31,11 +31,11 @@ console.log(planetDistanceFromSun(mars)); // A
 console.log(planetDistanceFromSun(venus)); // B
 console.log(planetDistanceFromSun(jupiter)); // C */
 
-// Sem setTimeout todos são síncronos e aparecem na forma 'padrão' na ordem que são chamados.
+// Sem setTimeout todos são síncronos e aparecem na forma 'padrão' na ordem que são chamados A, B, C.
 
 
 //QUESTÃO 2
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+/* const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
 const mars = {
@@ -64,7 +64,21 @@ const jupiter = {
 
 console.log(planetDistanceFromSun(mars)); // A
 setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
-setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C */
 
 // A, C, B -> setTimeoute da C está para executar em 2ms e da B está em 3. A está síncrona.
 
+//Questão 3
+const print = (marsParam) => console.log("Returned planet: ", marsParam);
+const getPlanet = () => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    setTimeout(() => print(mars), 4000);
+  };
+  
+  getPlanet(); // imprime Marte depois de 4 segundos
