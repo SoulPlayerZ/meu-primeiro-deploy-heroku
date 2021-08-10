33ade48865a9db5cda4994d0fb2ba7b4a1078ce3
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-10 18:25:35 -0300

    Exercicios do dia BLOCO_9.1 QUestao 2

--- bloco_9/exercicios_do_dia/exercicios_do_dia.js
+++ bloco_9/exercicios_do_dia/exercicios_do_dia.js
@@ -1,4 +1,6 @@
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//Questão 1
+
+/* const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
 const mars = {
@@ -27,7 +29,42 @@ const jupiter = {
 
 console.log(planetDistanceFromSun(mars)); // A
 console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+console.log(planetDistanceFromSun(jupiter)); // C */
 
 // Sem setTimeout todos são síncronos e aparecem na forma 'padrão' na ordem que são chamados.
+
+
+//QUESTÃO 2
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+// A, C, B -> setTimeoute da C está para executar em 2ms e da B está em 3. A está síncrona.
 
