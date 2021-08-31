24d82a55f70dc5a50c588229a5b2482db069ad43
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-31 15:32:27 -0300

    Fixacao 11.2 Exercicio ORDER

--- front-end/dia_2/fixacao/fixacao_dia-2/src/Image.js
+++ front-end/dia_2/fixacao/fixacao_dia-2/src/Image.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class ImageCat extends React.Component {
+class Image extends React.Component {
   render() {
     return <img src={this.props.source} alt={this.props.alternativeText} />;
   }
 }
 
-export default ImageCat;
+export default Image;
