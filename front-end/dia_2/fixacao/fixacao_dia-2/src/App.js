81720f6da75c2514f107248bb75408969fe40e87
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-08-31 15:16:35 -0300

    Fixacao 11.2 - exercicio foto do gato

---
+++ front-end/dia_2/fixacao/fixacao_dia-2/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageCat from './ImageCat';
+
+function App() {
+  return (
+    <ImageCat source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
+   );
+}
+
+export default App;
